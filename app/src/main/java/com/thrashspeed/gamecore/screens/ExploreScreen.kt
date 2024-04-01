27d6a7c4f4616bc568b3e439c005d67876e07d75
Adrian <agarciagale@gmail.com>
2024-04-01 22:25:44 +0200

    guardar estado del sroll en las listas por cada tab, y empezar consultas de plataformas

--- app/src/main/java/com/thrashspeed/gamecore/screens/ExploreScreen.kt
+++ app/src/main/java/com/thrashspeed/gamecore/screens/ExploreScreen.kt
@@ -2,6 +2,7 @@ package com.thrashspeed.gamecore.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -10,20 +11,31 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
+import com.thrashspeed.gamecore.R
 import com.thrashspeed.gamecore.data.model.GameItem
 import com.thrashspeed.gamecore.utils.igdb.IgdbHelperMethods
 import com.thrashspeed.gamecore.utils.igdb.IgdbImageSizes
@@ -35,13 +47,66 @@ fun ExploreScreen(navController: NavController) {
 
 @Composable
 fun ExploreScreenBodyContent(navController: NavController, viewModel: ExploreViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
-    val popularGamesState = viewModel.popularGames.collectAsState()
-
-    GameList(games = popularGamesState.value)
+    SliderWithTabs(viewModel)
 }
 
 @Composable
-fun GameList(games: List<GameItem>) {
+fun SliderWithTabs(viewModel: ExploreViewModel) {
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+
+    val tabs = listOf(LocalContext.current.getString(R.string.exploreTabs_games), LocalContext.current.getString(R.string.exploreTabs_platforms))
+
+//    val scrollStates = rememberSaveable { mutableStateOf(listOf(rememberLazyListState(), rememberLazyListState())) }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TabRow(
+            selectedTabIndex = selectedTabIndex,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex])
+                )
+            }
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    text = { Text(title) },
+                    selected = selectedTabIndex == index,
+                    onClick = { selectedTabIndex = index }
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // Content based on the selected tab index
+        when (selectedTabIndex) {
+            0 -> GamesExploreContent(viewModel)
+            1 -> Text("Content for platforms")
+        }
+    }
+}
+
+
+@Composable
+fun GamesExploreContent(viewModel: ExploreViewModel) {
+    val gamesState by remember(viewModel) { viewModel.popularGames }.collectAsState()
+
+    Column (
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = LocalContext.current.getString(R.string.explore_famousGames),
+            style = MaterialTheme.typography.headlineMedium.copy(fontSize = 20.sp),
+            modifier = Modifier.padding(8.dp)
+        )
+        GamesHorizontalList(games = gamesState)
+    }
+}
+
+@Composable
+fun GamesHorizontalList(games: List<GameItem>) {
     LazyRow {
         items(games) { gameItem ->
             GameListItem(gameItem)
